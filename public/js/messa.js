a4882a0122b539cef6b3862219ad0a5f8343c938
lejandro83 <lejandro83@gmail.com>
2017-01-27 09:44:16 -0600

    Adds mediaqueries for used res

--- public/js/messa.js
+++ public/js/messa.js
@@ -1,1 +1,6 @@
-$('#iPhoneWhite').addClass('animated bounceOutLeft');
+document.addEventListener("DOMContentLoaded", function(event) { 
+  //do work
+  $("#mailing").click(function(){
+   $("#usr-EMAIL").focus();
+  });
+});
